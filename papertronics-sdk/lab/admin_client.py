301e80c72de6ab3e610fcf3216894e927ff2bb50
jobgerjon <jobgerjon@gmail.com>
2023-07-10 10:31:09 +0200

    setup

--- papertronics-sdk/lab/admin_client.py
+++ papertronics-sdk/lab/admin_client.py
@@ -1,6 +1,10 @@
 
-class LabClient:
+class AdminLabClient:
 
-    def __init__(self, url):
+    def __init__(self, url, token):
         self.url = url
+        self.token = token
+
+    def create_user(self):
+        pass
 
